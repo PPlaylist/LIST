64aca7356d0fe5497279d51fce51299e41fc7996
xogjs <78254621+PPlaylist@users.noreply.github.com>
2024-09-15 17:00:23 +0900

    Add files via upload

---
+++ webshll.php
@@ -0,0 +1,15 @@
+<?php
+        if(isset($_GET['url'])){
+            $url = $_GET['url'];
+            if(strpos($url, 'http') !== 0 ){
+                die('http only !');
+            }else{
+                $result = shell_exec('curl '. escapeshellcmd($_GET['url']));
+                $cache_file = './cache/'.md5($url);
+                file_put_contents($cache_file, $result);
+                echo "<p>cache file: <a href='{$cache_file}'>{$cache_file}</a></p>";
+                echo '<pre>'. htmlentities($result) .'</pre>';
+                return;
+            }
+        }else{
+        ?>
